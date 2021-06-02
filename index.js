97dfdeb241dc339e08742756f9970b3514f8d836
Cory <corystephens012@gmail.com>
2021-06-02 17:03:02 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names=["John", "Mark", "Luke", "Mary", "Chris"];
+const thankYou = []
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++){
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+
+function countDown(n){
+    while (n >=0){
+        console.log(n)
+        n--
+    }
+}
